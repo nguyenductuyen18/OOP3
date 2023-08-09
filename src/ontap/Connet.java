6d8e17be8b51fbb6369cc589d1b84024f8b4ee78
nguyenductuyen18 <nguyenductuyen2k4@gmail.com>
2023-08-09 15:38:57 +0700

    ontap

---
+++ src/ontap/Connet.java
@@ -0,0 +1,13 @@
+package ontap;
+
+import JDBC.bt.ConnectJDBC;
+
+import java.sql.Connection;
+
+public class Connet {
+    private String hostName = "localhost:3306";
+    private String dbName = "product_manager_database";
+    private String users ="root";
+    private String passwer = "1";
+
+}
